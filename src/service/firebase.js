a29bcb4e688ae6ad84f3bda81717e8c773b3b37f
홍현경 <nowhhk@gmail.com>
2021-03-10 01:02:43 +0900

    firebase auth

---
+++ src/service/firebase.js
@@ -0,0 +1,7 @@
+const firebaseConfig = {
+  apiKey: process.env,
+  authDomain: "thumbnail-maker-1fbea.firebaseapp.com",
+  projectId: "thumbnail-maker-1fbea",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
